d2fb1b3c49bc7e1a821591a0149bbce4d2c52e76
Chanon Panpila <nonpanpila@gmail.com>
2018-05-09 02:00:56 +0700

    :tada: initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  mode: "production",
+  context: path.join(__dirname, "./src"),
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "index.html",
+      template: "public/index.html"
+    })
+  ],
   entry: {
-    'app': './src/main.ts',
+    app: "./main.ts"
   },
   output: {
-    filename: '[name].js'
+    filename: "[name].js"
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
-      { test: /.tsx?$/, loader: 'ts-loader' },
+      { test: /.tsx?$/, loader: "ts-loader" },
       {
         test: /\.js$/,
         use: ["source-map-loader"],
@@ -19,7 +30,6 @@ module.exports = {
       }
     ]
   },
-  devtool:'source-map',
-  devServer: {
-  }
-}
+  devtool: "source-map",
+  devServer: {}
+};
