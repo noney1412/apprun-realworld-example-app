c7fb14f5e2bf1bf9b969af21d9808dc507e9e8aa
Chanon Panpila <nonpanpila@gmail.com>
2018-05-09 02:44:04 +0700

    :white_check_mark: adding .prettierrc

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import app from "apprun";
+import app from 'apprun';
 
-const model = "Hello world - AppRun !";
+const model = 'Hello world - AppRun !';
 
 const view = state => (
   <div>
     <h1>{state}</h1>
+    <input oninput={e => app.run('input', e)} />
   </div>
 );
 
-const update = {};
+const update = {
+  input: (state, e)
+};
 
-app.start("my-app", model, view, update);
+app.start('my-app', model, view, update);
